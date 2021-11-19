569d013e6e88cd381dde40b4fc59b950b21f8b23
Andrea Rufo <a.rufo@axio.studio>
2021-11-19 18:59:39 +0100

    Bootstrap

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Andrea Rufo, freelance full stack web developer personal portfolio',
+    'description' => 'Sito web e portofolio di Andrea Rufo, full stack web developer. Programmazione di siti web e applicazioni. Specializzato in Larave, Vue, PWA, WordPress e webdesign.',
+    'collections' => [],
+];
