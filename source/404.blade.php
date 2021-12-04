fa358ae22a2e4016eb11b0d3a0b01c0ccbeb0267
Andrea Rufo <a.rufo@axio.studio>
2021-12-04 06:07:20 +0100

    Typo in 404

--- source/404.blade.php
+++ source/404.blade.php
@@ -4,7 +4,7 @@
 
 <h1>Contenuto non trovato</h1>
 <h2>Hey, questo è un 404. 💀</h2>
-<p>Cosa stavi cercando?</p>
-<p>Sembra proprio che il contenuto non ci sia o che sia cambiato... Se ti serve qualcosa in particoalare, non esitare: contattami!</p>
+<p>Cosa stavi cercando? 😖</p>
+<p>Sembra proprio che il contenuto non ci sia o che sia cambiato... Se ti serve qualcosa in particolare, non esitare: contattami!</p>
 
 @endsection
